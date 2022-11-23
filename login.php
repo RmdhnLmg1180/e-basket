5670e238b20099d0472599ac131202d01827494f
RmdhnLmg1180 <vmcxs12@gmail.com>
2022-11-23 22:49:21 +0700

    menambahkan index dan pemesanan

--- login.php
+++ login.php
@@ -82,3 +82,7 @@ if (isset($_POST['login'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
+<?php
+mysqli_close($conn);
+ob_end_flush();
+?>
