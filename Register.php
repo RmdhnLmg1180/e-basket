db42a3593bd7b8e900531fb9f164b844f0ee6504
RmdhnLmg1180 <vmcxs12@gmail.com>
2022-11-24 14:54:29 +0700

    mengisi konten di halaman home dan membuat form pemesanan

--- Register.php
+++ Register.php
@@ -103,7 +103,3 @@ if (isset($_POST['submit'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
   </body>
 </html>
-<?php 
-mysqli_close($conn);
-ob_end_flush();
-?>
